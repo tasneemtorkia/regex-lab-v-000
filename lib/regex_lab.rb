3e17ba89b0091fe3420dbeda22ffe005234af8fa
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-15 16:46:18 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,11 +16,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if text.match(/[A-Z]\w+/)
-  return true 
-else 
-  false
-end 
+text.match(/[A-Z]\w+/)
 end
 
 def valid_phone_number?(phone)
