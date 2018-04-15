3cea50222baff603e7c7803619204e0d2b324731
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-15 15:14:25 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\A[aeiouAEIOU]/)
-    return true 
-  end 
+   word.match(/\A[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
