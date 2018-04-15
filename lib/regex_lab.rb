3af02f98a44b50803c9b39143b9d524f08dcb866
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-15 17:08:10 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,8 +26,8 @@ end
 def valid_phone_number?(phone)
 valid_numbers = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
 if phone == valid_numbers
-  return true
+  return false
 else
-  false
+  true
 end 
 end
